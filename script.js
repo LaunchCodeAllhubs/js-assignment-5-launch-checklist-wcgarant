07f99583c6553691140cda4ab9def73787470878
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-07-26 23:50:21 -0400

    myfetch working

--- script.js
+++ script.js
@@ -26,5 +26,4 @@ console.log("click button")
 //        console.log(listedPlanets);
 //        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
 //    })
-   pickPlanet("hellopi");
 });
