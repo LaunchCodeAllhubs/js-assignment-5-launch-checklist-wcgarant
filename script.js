545f52bdb47d06f3682f9c0b26544429d2853af4
wcgarant <107452402+wcgarant@users.noreply.github.com>
2023-07-27 00:05:23 -0400

    addDestinationInfo working

--- script.js
+++ script.js
@@ -18,12 +18,14 @@ console.log("click button")
     })
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-//    listedPlanetsResponse.then(function (result) {
-//        listedPlanets = result;
-//        console.log(listedPlanets);
-//    }).then(function () {
-//        console.log(listedPlanets);
+   let listedPlanetsResponse = myFetch();
+   listedPlanetsResponse.then(function (result) {
+       listedPlanets = result;
+       console.log(listedPlanets);
+   }).then(function () {
+       console.log(listedPlanets);
 //        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-//    })
+    missionPlanet = pickPlanet(listedPlanets);
+    addDestinationInfo(document, missionPlanet.name, missionPlanet.diameter, missionPlanet.star, missionPlanet.distance, missionPlanet.moons, missionPlanet.image);
+   })
 });
